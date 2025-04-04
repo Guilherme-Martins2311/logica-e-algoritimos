cce7bfb6b40788d7c7bb577050c1053ff8ba4c87
Guilherme-Martins2311 <guilhermemartins2311@gmail.comclear>
2025-04-04 12:35:31 -0300

    filmes

---
+++ atividade04/filme.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    int opcao, leve, intenso, terror, acao;
+
+    printf("Escolha um tipo de filme: \n");
+    printf("1 - Leve \n");
+    printf("2 - intenso \n");
+    scanf("%u", &opcao);
+
+    if(opcao == 1){
+        printf("Escolha um genero de filme leve:\n");
+        printf("1 - Comedia\n");
+        printf("2 - animacao\n");
+        scanf("%u", &leve);
+
+        if(leve == 1){
+            printf("O filme recomendado eh:\nGente Grande\n");
+        }
+        else if(leve == 2){
+            printf("O filme recomendado eh:\nCarros\n");
+    }
+    }
+
+    if(opcao == 2){
+        printf("Escolha um genero de filme intenso:\n");
+        printf("1 - terror\n");
+        printf("2 - acao\n");
+        scanf("%u", &intenso);
+
+        if(intenso == 1){
+            printf("Escolha um genero de filme de terro:\n");
+            printf("1 - sobrenatural\n");
+            printf("2 - psicologico\n");
+            scanf("%u", &terror);
+            if(terror == 1){
+                printf("O filme recomendado eh:\nInvocacao do Mal\n");
+            }
+            else if(terror = 2){
+                printf("O filme recomendado eh:\nUma noite de crime\n");
+            }
+            
+        } 
+        else if(intenso == 2){
+            printf("Escolha um genero de filme de acao:\n");
+            printf("1 - Super Heroi\n");
+            printf("2 - acao realista\n");
+            scanf("%u", &acao);
+            if(acao == 1){
+                printf("O filme recomendado eh:\nVingadores Ultimato\n");
+            }
+            else if(acao == 2){
+                printf("O filme recomendado eh:\nSem Remorso\n");
+            }
+
+        }
+    }
+  return 0;
+}
