43ed8c89443d26e1bcce87c0e9c1b68d14eed4a1
Guilherme-Martins2311 <guilhermemartins2311@gmail.com>
2025-04-11 18:47:54 -0300

    add notas

--- atividade05/notas.c
+++ atividade05/notas.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    int alunos, n;
-    float media, maior, menor, soma;
+    int alunos, n, aprovados = 0, opcao, posicao;
+    float media, maior, menor;
+    float soma = 0;
+    
     
     printf("Digite a quantidade de alunos:\n");
     scanf("%i", &alunos);
@@ -14,14 +16,56 @@ int main(int argc, char* argv[]){
     for(n = 0; n < alunos; n++){
         scanf("%f", &notas[n]);
     }
-    
 
+    for(n = 0; n < alunos; n++){
+        printf("Nota %i eh: %2.f\n", n, notas[n]);
+    }
+    
     for(n = 0; n < alunos; n++){
         soma += notas[n];
     }
 
     media = (soma / alunos);
-    printf("A media das notas dos alunos eh: %2.f\n", media);
+    printf("\nA media das notas dos alunos eh: %2.f\n", media);
+
+    maior = notas[0];
+
+    for(n = 0; n < alunos; n++){
+        if (notas[n] > maior){
+            maior = notas[n];
+        }
+    }
+
+    printf("A maior nota eh: %2.f\n", maior);
+
+    menor = notas[0];
+
+    for(n = 0; n < alunos; n++){
+        if (notas[n] < menor){
+            menor = notas[n];
+        }
+    }
+
+    printf("A menor nota eh: %2.f\n", menor);
+
+    for(n = 0; n < alunos; n++){
+        if (notas[n] >= 6){
+            aprovados++;
+        }
+    }
+    printf("A quantidade de alunos aprovados eh: %i\n", aprovados);
+
+    printf("Deseja consultar uma nota especifica?\n");
+    printf("1 - sim\n");
+    printf("2 - nao\n");
+    scanf("%i", &opcao);
+
+    if(opcao == 1){
+        printf("Digite a posicao da nota desejada entre 0 e %i:\n", alunos);
+        scanf("%i", &posicao);
+        printf("A nota escolhida eh: %2.f\n", notas[posicao]);
+    }
+    
 
     return 0;
 }
