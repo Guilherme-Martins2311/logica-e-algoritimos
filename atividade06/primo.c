557c568b1beb640c170b1ba6a35158451a1da3df
Guilherme-Martins2311 <guilhermemartins2311@gmail,com>
2025-05-22 11:17:14 -0300

    primo

--- atividade06/primo.c
+++ atividade06/primo.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool eh_primo(int numero){
+        if(numero <= 1){
+            return false;
+        }
+
+        if(numero == 2){
+            return true;
+        }
+
+        for(int i = 2; i * i <= numero; i++){
+            if(numero % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
 int main(int agrc, char* argv[]){
     int numero;
 
-    printf("Digite o numero que voce deseja verificar se eh um numero primo ou nao:\n");
+    printf("Digite um numero para verificar se eh primo ou nao:\n");
     scanf("%d", &numero);
 
+    if(eh_primo(numero)){
+        printf("%d eh um numero primo\n", numero);
+    } else {
+        printf("%d nao eh um numero primo\n", numero);
+    }
 
     return 0;
 }
---
+++ funcao/nota.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Aluno{
+    char nome[64];
+    float nota;
+};
+
+int main(int argc, char* argv[]){
+
+    int n = 5;
+    struct Aluno alunos[n];
+
+    printf("Digite o nome do aluno a:\n");
+    fgets(alunos[])
+
+
+    return 0;
+}
