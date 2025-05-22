847b7bb55a5c903d158fc01057697adaae889958
Guilherme-Martins2311 <guilhermemartins2311@gmail,com>
2025-05-22 12:15:09 -0300

    structs

--- funcao/nota.c
+++ funcao/nota.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
+#define TAM_NOME 64
+
 struct Aluno{
-    char nome[64];
+    char nome[TAM_NOME];
     float nota;
-};
+} Aluno;
+
+void imprimir_aluno(struct Aluno a){
+    printf("Nome: %s \tnota: %.2f\n", a.nome, a.nota);
+}
 
 int main(int argc, char* argv[]){
 
     int n = 5;
     struct Aluno alunos[n];
 
-    printf("Digite o nome do aluno a:\n");
-    fgets(alunos[])
+    for(int i = 0; i < n; i++){
+        printf("Digite o nome do aluno %d:\n", i);
+        fgets(alunos[i].nome, TAM_NOME, stdin);
+
+        printf("Digite a nota do aluno %d:\n", i);
+        scanf(" %f", &alunos[i].nota);
+
+    }
+
 
 
     return 0;
